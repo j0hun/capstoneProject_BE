3482b64f5fdd4b1605995632a1827f6f703e49ea
j0hun <dudgns3681@gmail.com>
2024-05-10 19:58:37 +0900

    Heroku 배포

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,16 +1,9 @@
 package com.example.demo;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition(
-		servers = {
-				@Server(url="/", description = "Default Server url")
-		}
-)
 public class DemoApplication {
 
 	public static void main(String[] args) {
